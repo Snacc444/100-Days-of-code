6f2ae37a86eb94c397e742a916dfe845dc95462b
Ayaan Raj <ayaanraj205@gmail.com>
2026-08-22 03:32:05 +0530

    Implement addition of two numbers in day1.c
    Added a program to input two numbers and display their sum.

--- day1.c
+++ day1.c
@@ -1,5 +1,15 @@
-#include<stdio.h>
-int main(){
-    printf("hi");
-    return 0;
-}
+//Write a program to input two numbers and display their sum.
+
+#include <stdio.h>
+
+int main() {
+  float a,b,sum;                     //The entered numbers can be either integer values or decimal
+  printf("Enter the first number:");
+  scanf("%f",&a);
+
+  printf("Enter the second number:");
+  scanf("%f",&b);
+  
+  sum= a + b;                //The sum will be either a decimal value or n integer value depending upon the numbers entered
+  printf("sum = %.2f\n",sum);
+  return 0;
